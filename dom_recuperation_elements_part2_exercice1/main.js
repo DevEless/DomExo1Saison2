6f5762713056bd039a327fa69f90c1a00038d98e
DevEless <qdelameilleure@gmaile.com>
2023-01-13 09:32:55 +0100

    fin

---
+++ dom_recuperation_elements_part2_exercice1/main.js
@@ -0,0 +1,7 @@
+let tarte = document.getElementsByClassName("tartine")[0].textContent
+console.log(tarte)
+
+let throat = document.getElementById('toast')[0].textContent
+console.log(throat)
+let tartineLast = document.getElementsByClassName('tartine')
+console.log(tartineLast.lastElementChild)
